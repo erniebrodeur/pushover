ea0ba4e0cac09f883d006c4fecdd45682015431c
Ernie Brodeur <ebrodeur@ujami.net>
2012-08-27 01:02:22 -0700

    CLI works without the optional title.

--- lib/pushover/optparser.rb
+++ lib/pushover/optparser.rb
@@ -7,6 +7,10 @@ module Pushover
       @options = {}
 
       on("-V", "--version", "Print version") { |version| @options[:version] = true}
+      on("-t", "--token TOKEN", "Set your identity token.") { |o| @options[:token] = o}
+      on("-a", "--app-key APPKEY", "Set the receiving application key.") { |o| @options[:appkey] = o}
+      on("-m", "--message MESSAGE", "The message to be sent.") { |o| @options[:message] = o}
+#      on("-T", "--title TITLE", "Set the title of the notification (optional).") { |o| @options[:title] = o}
     end
 
     # This will build an on/off option with a default value set to false.
@@ -33,8 +37,10 @@ module Pushover
       end
     end
 
-    def [](k)
-      @options[k]
+    def [](k = nil)
+      return @options[k] if k
+      return @options if @options.any?
+      nil
     end
 
     def []=(k,v)
