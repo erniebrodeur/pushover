9b0dfdf521241b4f784a9a5b35891d89b2d88b45
Ernie Brodeur <ebrodeur@ujami.net>
2018-09-06 16:37:02 -0700

    Specced out response and built code.

--- lib/pushover/response.rb
+++ lib/pushover/response.rb
@@ -17,10 +17,24 @@ module Pushover
       @errors = []
     end
 
-    def process; end
+    def process
+      process_body
+      process_headers
+    end
 
-    def process_body; end
+    def process_body
+      body = Oj.load original[:body]
 
-    def process_headers; end
+      @status = body[:status]
+      @request = body[:request]
+      @receipt = body[:receipt] if body[:receipt]
+      @errors = body[:errors] if body[:errors]
+    end
+
+    def process_headers
+      @limit = original.headers['X-Limit-App-Limit']
+      @remaining = original.headers['X-Limit-App-Remaining']
+      @reset = original.headers['X-Limit-App-Reset']
+    end
   end
 end
--- spec/lib/pushover/response_spec.rb
+++ spec/lib/pushover/response_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 module Pushover
   describe Response do
     let(:body) { { "status": 1, "request": "647d2300-702c-4b38-8b2f-d56326ae460b" } }
+    let(:receipt_body) { { "status": 1, "request": "647d2300-702c-4b38-8b2f-d56326ae460b", "receipt": 'abcdefg' } }
     let(:error_body) { { "user": "invalid", "errors": ["user identifier is invalid"], "status": 0, "request": "5042853c-402d-4a18-abcb-168734a801de" } }
     let(:headers) do
       {
@@ -11,6 +12,10 @@ module Pushover
         "X-Limit-App-Reset":     1_393_653_600
       }
     end
+    let(:excon_response) { Excon::Response.new(body: Oj.dump(body), status: 200, headers: headers) }
+    let(:response) { described_class.create original: excon_response }
+
+    it { is_expected.to be_a_kind_of Creatable }
 
     it { expect(described_class).to be_a_kind_of(Class) }
 
@@ -23,42 +28,71 @@ module Pushover
       it { is_expected.to have_attributes(request: nil) }
       it { is_expected.to have_attributes(reset: nil) }
       it { is_expected.to have_attributes(status: a_kind_of(Numeric)) }
-      it { is_expected.to have_attributes(user: nil) }
     end
 
     describe "Instance Signatures" do
-      it { is_expected.to respond_to(:original=).with(1).argument }
       it { is_expected.to respond_to(:process).with(0).argument }
       it { is_expected.to respond_to(:process_body).with(0).argument }
       it { is_expected.to respond_to(:process_headers).with(0).argument }
     end
 
-    describe '::original=' do
-      context "when the argument is not an Excon::Response" do
-        it "is expected to raise argument error"
+    describe '::process' do
+      shared_examples 'process_verb' do |verb|
+        it "is expected to call process_#{verb}" do
+          allow(response).to receive("process_#{verb}".to_sym)
+          response.process
+          expect(response).to have_received("process_#{verb}".to_sym)
+        end
       end
 
-      it "is expected to set @original the argument"
-    end
-
-    describe '::process' do
-      it "is expected to call process_body"
-      it "is expected to call process_headers"
+      include_examples 'process_verb', 'body'
+      include_examples 'process_verb', 'headers'
     end
 
     describe '::process_body' do
-      it "is expected to set errors"
-      it "is expected to set original"
-      it "is expected to set receipt"
-      it "is expected to set request"
-      it "is expected to set status"
-      it "is expected to set user"
+      context "when a receipt is provided" do
+        let(:body) { receipt_body }
+
+        it "is expected to set receipt" do
+          expect { response.process_body }.to change(response, :receipt).to body[:receipt]
+        end
+      end
+
+      context "when a errors is provided" do
+        let(:body) { error_body }
+
+        it "is expected to set errors" do
+          expect { response.process_body }.to change(response, :errors).to body[:errors]
+        end
+      end
+
+      it "is expected to call Oj.load" do
+        allow(Oj).to receive(:load).and_call_original
+        response.process_body
+        expect(Oj).to have_received(:load)
+      end
+
+      it "is expected to set status" do
+        expect { response.process_body }.to change(response, :status).to body[:status]
+      end
+
+      it "is expected to set request" do
+        expect { response.process_body }.to change(response, :request).to body[:request]
+      end
     end
 
     describe '::process_headers' do
-      it "is expected to set limit to X-Limit-App-Limit"
-      it "is expected to set remaing to X-Limit-App-Remaining"
-      it "is expected to set reset to X-Limit-App-Reset"
+      it "is expected to set limit to X-Limit-App-Limit" do
+        expect { response.process_headers }.to change(response, :limit).to headers[:'X-Limit-App-Limit']
+      end
+
+      it "is expected to set remaing to X-Limit-App-Remaining" do
+        expect { response.process_headers }.to change(response, :remaining).to headers[:'X-Limit-App-Remaining']
+      end
+
+      it "is expected to set reset to X-Limit-App-Reset" do
+        expect { response.process_headers }.to change(response, :reset).to headers[:'X-Limit-App-Reset']
+      end
     end
   end
 end
