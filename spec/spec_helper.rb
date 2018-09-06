4e7326f909cf3a50d4e23b719378ad945b6c501e
Ernie Brodeur <ebrodeur@ujami.net>
2018-09-05 22:22:53 -0700

    Update for focus and persistence.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,12 @@ SimpleCov.formatters = [
   SimpleCov::Formatter::HTMLFormatter
 ]
 
+RSpec.configure do |config|
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+  config.example_status_persistence_file_path = "tmp/examples.txt"
+end
+
 SimpleCov.start do
   add_filter "/spec/"
 end
