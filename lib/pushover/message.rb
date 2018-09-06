d4cbfa11b1322dc27602013d99f89f029f9e4a42
Ernie Brodeur <ebrodeur@ujami.net>
2018-09-05 22:22:13 -0700

    Almost complete (3 yellow)

--- lib/pushover/message.rb
+++ lib/pushover/message.rb
@@ -1,28 +1,37 @@
+# Pushover interface
 module Pushover
-  # Message object
   class Message
-    attr_accessor :token
-    attr_accessor :user
-    attr_accessor :device
-    attr_accessor :timestamp
-    attr_accessor :title
-    attr_accessor :message
-    attr_accessor :priority
-    attr_accessor :url
-    attr_accessor :url_title
-    attr_accessor :sound
+    include Creatable
+
+    attribute name: 'attachment'
+    attribute name: 'device', kind_of: String
+    attribute name: 'html', kind_of: TrueClass
+    attribute name: 'message', kind_of: String
+    attribute name: 'priority', kind_of: String
+    attribute name: 'sound', kind_of: String
+    attribute name: 'timestamp', kind_of: Numeric
+    attribute name: 'title', kind_of: String
+    attribute name: 'token', kind_of: String
+    attribute name: 'response', kind_of: Pushover::Response
+    attribute name: 'url_title', kind_of: String
+    attribute name: 'url', kind_of: String
+    attribute name: 'user', kind_of: String
 
     def initialize
-      @token = ''
-      @user = ''
-      @device = ''
-      @title = ''
-      @message = ''
-      @priority = ''
-      @url = ''
-      @url_title = ''
-      @sound = ''
-      @timestamp = DateTime.now
+      @timestamp = Time.now.to_i
+    end
+
+    def push
+      raise ArgumentError, 'user is a required parameter' unless user
+      raise ArgumentError, 'token is a required parameter' unless token
+      raise ArgumentError, 'message is a required parameter' unless message
+
+      body = {}
+
+      attributes.each { |e| body.store e[:name], instance_variable_get("@#{e[:name]}") if instance_variable_get("@#{e[:name]}") }
+
+      response = Api.connection.post path: '1/messages.json', body: Oj.dump(body)
+      response
     end
   end
 end
--- spec/lib/pushover/message_spec.rb
+++ spec/lib/pushover/message_spec.rb
@@ -2,46 +2,71 @@ require 'spec_helper'
 
 module Pushover
   describe Message do
-    # let(:param_list) do
-    #   [
-    #     'message',
-    #     'title',
-    #     'token',
-    #     'user',
-    #     'device',
-    #     'url',
-    #     'url_title',
-    #     'priority',
-    #     'timestamp',
-    #     'sound'
-    #   ]
-    # end
-
-    it { is_expected.to have_attributes(token: a_kind_of(String)) }
-    it { is_expected.to have_attributes(user: a_kind_of(String)) }
-    it { is_expected.to have_attributes(device: a_kind_of(String)) }
-    it { is_expected.to have_attributes(title: a_kind_of(String)) }
-    it { is_expected.to have_attributes(message: a_kind_of(String)) }
-    it { is_expected.to have_attributes(timestamp: a_kind_of(DateTime)) }
-    it { is_expected.to have_attributes(priority: a_kind_of(String)) }
-    it { is_expected.to have_attributes(url: a_kind_of(String)) }
-    it { is_expected.to have_attributes(url_title: a_kind_of(String)) }
-    it { is_expected.to have_attributes(sound: a_kind_of(String)) }
-
-    describe "#create" do
-      # specify { expect(described_class).to respond_to(:image_deployable?).with(1).arguments }
-
-      it "will return a new message object"
+    describe "Attributes" do
+      it { is_expected.to have_attributes(attachment: nil) }
+      it { is_expected.to have_attributes(device: nil) }
+      it { is_expected.to have_attributes(html: nil) }
+      it { is_expected.to have_attributes(message: nil) }
+      it { is_expected.to have_attributes(priority: nil) }
+      it { is_expected.to have_attributes(response: nil) }
+      it { is_expected.to have_attributes(sound: nil) }
+      it { is_expected.to have_attributes(timestamp: a_kind_of(Numeric)) }
+      it { is_expected.to have_attributes(title: nil) }
+      it { is_expected.to have_attributes(token: nil) }
+      it { is_expected.to have_attributes(url_title: nil) }
+      it { is_expected.to have_attributes(url: nil) }
+      it { is_expected.to have_attributes(user: nil) }
     end
 
-    describe ".post" do
-      it "will return a new response object"
+    it { is_expected.to respond_to(:push).with(0).argument }
 
-      it "will call Api::post with to_hash"
+    describe "::push" do
+      let(:message) { described_class.new }
+      let(:required_params) { { user: '1234', token: '1234', message: 'abcd' } }
+      let(:working_message) { described_class.create required_params }
+      let(:excon_connection) { Excon.new Api.url }
+      let(:excon_response) { Excon::Response.new }
 
-      context "when the argument includes a param that is not valid" do
-        it "is expected to raise a RuntimeError exception with message /param/"
+      before do
+        allow(Api).to receive(:connection).and_return excon_connection
+        allow(excon_connection).to receive(:post).and_return excon_response
       end
+
+      shared_examples 'required_param' do |param|
+        context "when #{param} is not set" do
+          before { working_message.instance_variable_set "@#{param}".to_sym, nil }
+
+          it { expect { working_message.push }.to raise_error ArgumentError, /#{param} is a required parameter/ }
+        end
+      end
+
+      shared_examples 'extra_param' do |param|
+        context "when a #{param} is provided" do
+          before { working_message.instance_variable_set "@#{param}".to_sym, 'not_nil' }
+
+          it "is expected to add the #{param} to the query" do
+            working_message.push
+            expect(excon_connection).to have_received(:post).with(body: a_string_including(param.to_s), path: '1/messages.json')
+          end
+        end
+      end
+
+      %i[message token user].each do |param|
+        include_examples 'required_param', param
+      end
+
+      %i[attachment device html priority sound title url_title url].each do |param|
+        include_examples 'extra_param', param
+      end
+
+      it 'is expected to call Api.connection.get with query set' do
+        working_message.push
+        expect(excon_connection).to have_received(:post).with(body: a_kind_of(String), path: '1/messages.json')
+      end
+
+      it "is expected to call Response.create with the response set to original"
+      it "is expected to call response.process to populate the object"
+      it "is expected to return a Response object"
     end
   end
 end
