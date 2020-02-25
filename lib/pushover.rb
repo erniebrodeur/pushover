3c12645f48ad493ba4483be1b3e578dcbdac9617
Sprocket Security <sprocketsecurity@users.noreply.github.com>
2020-02-25 09:06:30 -0600

    remove freeze

--- lib/pushover.rb
+++ lib/pushover.rb
@@ -9,7 +9,7 @@ require 'pushover/version'
 # pushover interface for ruby
 module Pushover
   # headers to include in every request.
-  HEADERS = { 'Content-Type' => 'application/json', 'User-Agent' => "pushover (ruby gem) v#{VERSION}" }.freeze
+  HEADERS = { 'Content-Type' => 'application/json', 'User-Agent' => "pushover (ruby gem) v#{VERSION}" }
   # excon connection to use for every request.
-  Excon = Excon.new('https://api.pushover.net', headers: HEADERS).freeze
+  Excon = Excon.new('https://api.pushover.net', headers: HEADERS)
 end
